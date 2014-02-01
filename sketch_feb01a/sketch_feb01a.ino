e289542c8d9a3b5daf0f6983417339f404f57d75
Shivan <shivankaul.1993@gmail.com>
2014-02-01 15:58:57 -0500

    updated Arduino software

---
+++ sketch_feb01a/sketch_feb01a.ino
@@ -0,0 +1,87 @@
+  const int switchPin = 2;// switch input; redundant
+    const int right_motor1Pin = 3;// Right motor: H-bridge leg 1 (pin 2, 1A)
+    const int right_motor2Pin = 4;// Right motor: H-bridge leg 2 (pin 7, 2A)
+    const int enablePin = 9;// H-bridge enable pin
+    const int left_motor1Pin = 6; // Left motor: H-bridge leg (pin 15, 4A)
+    const int left_motor2Pin = 7; // Left motor: H-bridge leg ()
+      
+    
+     void setup() {
+      // set the switch as an input (redundant; delete?):
+      pinMode(switchPin, INPUT);  
+  
+      // set all the other pins you're using as outputs:
+      pinMode(right_motor1Pin, OUTPUT); 
+      pinMode(right_motor2Pin, OUTPUT);
+      pinMode(left_motor1Pin,OUTPUT); 
+      pinMode(left_motor2Pin,OUTPUT); 
+      pinMode(enablePin, OUTPUT);
+      
+      
+      // set enablePin high so that motor can turn on:
+      delay(5000);
+      digitalWrite(enablePin, HIGH); 
+    }
+    
+  void loop() {
+      // Testing locomotion functions
+      if(digitalRead(switchPin)==HIGH)
+      upOrDown(true,5000);//move forward for 5 seconds and stop
+      upOrDown(false,5000);//backward for 5 seconds and stop
+      leftOrRight(true,5000);//keep turning right for 5 seconds and stop
+      leftOrRight(false,5000);//keep turning left for 5 seconds and stop  
+      }
+      else{
+       brake(tr) 
+        
+      }
+      }
+    
+    
+    void brake(boolean applyBrakes){
+      if(applyBrakes){
+        digitalWrite(right_motor1Pin, LOW);   
+        digitalWrite(right_motor2Pin, LOW);  
+        digitalWrite(left_motor1Pin, LOW);
+        digitalWrite(left_motor2Pin, LOW);      
+      }   
+    }
+    
+    void leftOrRight(boolean right, int time){
+       if(right){
+        digitalWrite(right_motor1Pin, HIGH);   // set leg 1 of the H-bridge low
+        digitalWrite(right_motor2Pin, LOW);  // set leg 2 of the H-bridge high
+        digitalWrite(left_motor1Pin, LOW);
+        digitalWrite(left_motor2Pin, HIGH); 
+        delay(time);
+        brake(true);   
+      }
+    else {
+        digitalWrite(right_motor1Pin, LOW);   // set leg 1 of the H-bridge low
+        digitalWrite(right_motor2Pin, HIGH);  // set leg 2 of the H-bridge high
+        digitalWrite(left_motor1Pin, HIGH);
+        digitalWrite(left_motor2Pin, LOW);
+        delay(time);
+        brake(true);  
+    }   
+    }
+    
+    void upOrDown(boolean up, int time){    
+      if(up){
+        digitalWrite(right_motor1Pin, LOW);   // set  1 of the H-bridge low
+        digitalWrite(right_motor2Pin, HIGH);  // set leg 2 of the H-bridge high
+        digitalWrite(left_motor1Pin, LOW);
+        digitalWrite(left_motor2Pin, HIGH);   
+        delay(time);
+        brake(true);   
+      }
+      
+    else {
+        digitalWrite(right_motor1Pin, HIGH);   // set leg 1 of the H-bridge low
+        digitalWrite(right_motor2Pin, LOW);  // set leg 2 of the H-bridge high
+        digitalWrite(left_motor1Pin, HIGH);
+        digitalWrite(left_motor2Pin, LOW);
+        delay(time);
+        brake(true);  
+      } 
+    }
